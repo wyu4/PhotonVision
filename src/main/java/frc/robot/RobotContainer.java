73649e8c4c492f2842d92ceee311c078d9bf704c
Wilson <wilsonyu657@gmail.com>
2024-11-08 09:29:22 -0500

    Added Vision Subsystem to Robot container.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Subsystems.VisionSubsystem;
 
 public class RobotContainer {
   public RobotContainer() {
+    new VisionSubsystem();
     configureBindings();
   }
 
