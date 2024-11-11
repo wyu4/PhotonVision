b7dc6a6b81b670c1bd6688a3f82bd616f86bf78e
Wilson <139521392+wyu4@users.noreply.github.com>
2024-11-11 09:59:56 -0500

    Estimate the robot's position based on april tags

--- src/main/java/frc/robot/Subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/Subsystems/VisionSubsystem.java
@@ -5,64 +5,108 @@
 package frc.robot.Subsystems;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.VisionConstants;
 
 public class VisionSubsystem extends SubsystemBase {
   private final PhotonCamera camera = new PhotonCamera(VisionConstants.CAMERA_NAME);
+  private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
+  private final Transform3d cameraToRobot = new Transform3d(VisionConstants.CAMERA_TO_ROBOT_X, VisionConstants.CAMERA_TO_ROBOT_Y, VisionConstants.CAMERA_TO_ROBOT_Z, new Rotation3d());
+  private final Field2d simulatedField = new Field2d();
 
   /** Creates a new VisionSubsystem. */
   public VisionSubsystem() {}
 
   @Override
   public void periodic() {
-    PhotonPipelineResult result = getLatestResult();
 
-    SmartDashboard.putNumber("Vision_Target_Yaw", 0);
-    SmartDashboard.putNumber("Vision_Target_Pitch", 0);
-    SmartDashboard.putNumber("Vision_Target_Area", 0);
-    SmartDashboard.putNumber("Vision_Target_Skew", 0);
-    SmartDashboard.putString("Vision_Target_Pose", "[]");
-    SmartDashboard.putString("Vision_Target_Corners", "[]");
+    //////////////////////////////////////////////////////////////////////
+    // PhotonVision
+    PhotonTrackedTarget target = getTarget();
 
-    if (result == null) {
-      SmartDashboard.putString("Vision_Target_Status", "Result not found.");
-      return;
+    if (target != null) {
+      SmartDashboard.putNumber("Vision_Target_ID", target.getFiducialId());
+      SmartDashboard.putNumber("Vision_Target_Yaw", target.getYaw());
+      SmartDashboard.putNumber("Vision_Target_Pitch", target.getPitch());
+      SmartDashboard.putNumber("Vision_Target_Area", target.getArea());
+      SmartDashboard.putNumber("Vision_Target_Skew", target.getSkew());
+
+      Pose3d estimatedPose = estimateRobotPose();
+      if (estimatedPose != null) {
+        simulatedField.setRobotPose(estimatedPose.toPose2d());
+        SmartDashboard.putData("PoseFromAprilTag", simulatedField);
+      }
     }
-
-    if (!result.hasTargets()) {
-      SmartDashboard.putString("Vision_Target_Status", "Result found, no targets found.");
-      return;
-    }
-
-    SmartDashboard.putString("Vision_Target_Status", "Result found, receiving target data.");
-    PhotonTrackedTarget target = result.getBestTarget();
-
-    SmartDashboard.putNumber("Vision_Target_ID", target.getFiducialId());
-    SmartDashboard.putNumber("Vision_Target_Yaw", target.getYaw());
-    SmartDashboard.putNumber("Vision_Target_Pitch", target.getPitch());
-    SmartDashboard.putNumber("Vision_Target_Area", target.getArea());
-    SmartDashboard.putNumber("Vision_Target_Skew", target.getSkew());
-    SmartDashboard.putString("Vision_Target_Pose", target.getBestCameraToTarget().toString());
-    SmartDashboard.putString("Vision_Target_Corners", target.getDetectedCorners().toString());
+    //////////////////////////////////////////////////////////////////////
   }
 
-  // public List<PhotonPipelineResult> getUnreadResults() {
-  //   return camera.getAllUnreadResults();
-  // }
-
-  public PhotonPipelineResult getLatestResult() {
-    // List<PhotonPipelineResult> unreadResults = getUnreadResults();
-    // if (unreadResults.isEmpty()) {
-    //   return null;
-    // }
-    // return getUnreadResults().get(unreadResults.size()-1);
+  /**
+   * Get the latest unread result.
+   * @return Latest result.
+   */
+  private PhotonPipelineResult getLatestResult() {
     return camera.getLatestResult();
+  }
+
+  /**
+   * Get the latest best apriltag target.
+   * @return Best target, null if not found.
+   */
+  public PhotonTrackedTarget getTarget() {
+    return getLatestResult().hasTargets() ? getLatestResult().getBestTarget() : null;
+  }
+
+  /**
+   * Get the target with the corresponding fiducial ID.
+   * @param fiducialId The attached ID
+   * @return Target, null if not found.
+   */
+  public PhotonTrackedTarget getTarget(int fiducialId) {
+    PhotonPipelineResult result = getLatestResult();
+    if (!result.hasTargets()) {
+      for (PhotonTrackedTarget target : result.getTargets()) {
+        if (target.getFiducialId() == fiducialId) {
+          return target;
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Estimates the robot's position based on a tracked april tag
+   * @return {@code Pose3d} containing an estimate robot position. Returns null if position could not be estimated.
+   */
+  public Pose3d estimateRobotPose() {
+    PhotonTrackedTarget target = getTarget();
+
+    if (target == null || target.getFiducialId() == -1) {
+      return null;
+    }
+
+    Optional<Pose3d> tagPose = aprilTagFieldLayout.getTagPose(target.getFiducialId());
+    if (tagPose.isEmpty()) {
+      return null;
+    }
+
+    return PhotonUtils.estimateFieldToRobotAprilTag(
+      target.getBestCameraToTarget(),
+      tagPose.get(),
+      cameraToRobot
+    );
   }
 }
